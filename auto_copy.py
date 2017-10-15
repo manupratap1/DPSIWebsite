0a0a2e8fe68338454ff021785dac9f57e77daf98
Tanavya <tanavya.dimri@gmail.com>
2017-10-15 19:58:03 +0530

    Added Responsive Navbar. Made footer responsive

--- auto_copy.py
+++ auto_copy.py
@@ -1,4 +1,5 @@
 #Use this file so that changes are reflected in all files
+
 import os
 directory = os.getcwd()
 with open(directory + "/" + "header_new.html") as f:
@@ -6,20 +7,17 @@ with open(directory + "/" + "header_new.html") as f:
 start_point = '<div class="nav">'
 end_point = '<!--top header inserted here-->'
 for filename in os.listdir(directory):
-    if (filename.endswith(".php") or filename.endswith("html")) and (filename != "header_new.html"):
-        with open(directory + "/" + filename, "r") as f:
-            txt = f.read().replace(".php", ".html")
+    if filename.endswith("html") and (filename != "header_new.html"):
         idx_beg = txt.find(start_point)
         idx_end = txt.find(end_point)
         txt = txt[:idx_beg] + to_replace + txt[idx_end:]
         if '<link rel="stylesheet" type="text/css" href="css/style.css">' not in txt:
             idx_beg = txt.find("<head>")
             txt = txt[:idx_beg+6] + '\n<link rel="stylesheet" type="text/css" href="css/style.css">' + txt[idx_beg+6:]
+        if '<script src="nav_script.js"></script>' not in txt:
+            idx_beg = txt.find("<head>")
+            txt = txt[:idx_beg+6] + '\n<script src="nav_script.js"></script>' + txt[idx_beg+6:]
+        txt = txt.replace('.active {\nbackground-color: white;\n}', '');
         with open(directory + "/" + filename, "w") as f:
             f.write(txt)
-        idx = 0
-        break
-        while idx < len(txt):
-            idx = txt.find("<style>")
-
 
