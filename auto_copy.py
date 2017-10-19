46b6cfc4152db3a322cd26ec16e59627afca7573
Tanavya <tanavya.dimri@gmail.com>
2017-10-20 01:16:20 +0530

    Made all pages responsive (to an extent)

--- auto_copy.py
+++ auto_copy.py
@@ -1,14 +1,44 @@
 #Use this file so that changes are reflected in all files
 import os
 directory = os.getcwd()
+"""
 with open(directory + "/" + "header_new.html") as f:
     to_replace = f.read()
-start_point = '<div class="nav">'
-end_point = '<!--top header inserted here-->'
+"""
+with open(directory + "/" + "footer.html") as f:
+    footer = f.read()
+#start_point = '<div class="nav">'
+#end_point = '<!--top header inserted here-->'
+start_point = '<table border=0 style="top-margin:0 px;width:100%;overflow: hidden;background:white;font-size:16px;font-family:Arial;line-height: 150%;text-align:justify;margin:0px;height:500px">'
+end_point = '</center>'
 for filename in os.listdir(directory):
-    if filename.endswith("html") and (filename != "header_new.html"):
+    if filename.endswith("html") and filename not in ["header_new.html" ,"footer.html", "index.html"]:
+
+        
         with open(directory + "/" + filename, "r") as f:
             txt = f.read()
+        idx_beg = txt.find(start_point)
+        """
+        if idx_beg == -1:
+            print "start point not found in", filename
+        else:
+            idx_end = txt.find(end_point, idx_beg)
+            if idx_beg == -1:
+                print "weird. not end point not found in", filename
+            else:
+                txt = txt[:idx_beg] + txt[idx_end:]
+        """
+        txt = txt.replace("</body>", "")
+        txt = txt.replace("<table width=1200px", "<table ")
+        txt = txt.replace("results.html", "#")
+        txt = txt.replace("</html>", "")
+        txt += "</body>\n</html>\n"
+        #txt = txt + footer + "</body>\n</html>\n"
+        txt = txt.replace("Arial", "Raleway")
+        txt = txt.replace("arial", "Raleway")
+        txt = txt.replace("ARIAL", "Raleway")
+
+        """
         idx_beg = txt.find(start_point)
         idx_end = txt.find(end_point)
         txt = txt[:idx_beg] + to_replace + txt[idx_end:]
@@ -29,6 +59,7 @@ for filename in os.listdir(directory):
         txt = txt.replace('<script src="js/jquery.min.js"></script>', " ")
         txt = txt.replace('<script type="text/javascript" src="./gallerycode/jquery-1.11.1.min.js"></script>', " ")
         txt = txt.replace('.active {\nbackground-color: white;\n}', ' ')
+        """
 
         with open(directory + "/" + filename, "w") as f:
             f.write(txt)
